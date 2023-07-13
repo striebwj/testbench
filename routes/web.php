786596cc43176d0851fd9e56ab380b6eefa8af21
Wade Striebel <wade@striebel.ca>
2023-07-12 23:00:56 -0400

    chore: install breeze

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
-use Illuminate\Pipeline\Pipeline;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use League\Pipeline\PipelineBuilder;
 
 /*
 |--------------------------------------------------------------------------
@@ -10,8 +9,8 @@ use League\Pipeline\PipelineBuilder;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
@@ -19,163 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Testing Laravel Pipelines
-// https://dev.to/abrardev99/pipeline-pattern-in-laravel-278p
-Route::get('/pipeline', function () {
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-    $order = new stdClass();
-    $order->status = 0;
-    $order->line_items = [
-        [
-            'id' => 1,
-            'name' => 'Item 1',
-            'price' => 100,
-            'quantity' => 2,
-        ],
-        [
-            'id' => 2,
-            'name' => 'Item 2',
-            'price' => 200,
-            'quantity' => 1,
-        ],
-    ];
-
-    $pipeline = app(Pipeline::class);
-    $response = $pipeline->send($order)
-        ->through([
-            function ($passable, $next) {
-                $functionStatus = 1; // pending
-
-                if ($passable->status > $functionStatus) {
-                    return $next($passable);
-                }
-
-                $passable->status = $functionStatus;
-                dump($passable->status);
-                return $next($passable);
-            },
-            function ($passable, $next) {
-                $functionStatus = 2; // calculating
-
-                if ($passable->status > $functionStatus) {
-                    return $next($passable);
-                }
-
-                $passable->status = $functionStatus;
-                dump($passable->status);
-
-                $passable->total = collect($passable->line_items)->sum(function ($item) {
-                    return $item['price'] * $item['quantity'];
-                });
-                return $next($passable);
-            },
-//            function ($passable, $next) {
-//                throw new Exception('Something went wrong');
-//            },
-        ])
-        ->then(function ($passable) {
-            return $passable;
-        });
-
-    dd($pipeline , $response);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-// Testing Laravel Pipelines with league/pipeline
-// https://github.com/thephpleague/pipeline
-Route::get('/pipeline/2', function () {
-
-    $order = new stdClass();
-    $order->status = 0;
-    $order->line_items = [
-        [
-            'id' => 1,
-            'name' => 'Item 1',
-            'price' => 100,
-            'quantity' => 2,
-        ],
-        [
-            'id' => 2,
-            'name' => 'Item 2',
-            'price' => 200,
-            'quantity' => 1,
-        ],
-    ];
-
-    $pipeline =  (new League\Pipeline\Pipeline)
-        ->pipe(function ($passable) {
-            $functionStatus = 1; // pending
-
-            if ($passable->status > $functionStatus) {
-                return $passable;
-            }
-
-            $passable->status = $functionStatus;
-            dump($passable->status);
-            return $passable;
-        })
-        ->pipe(function ($passable) {
-            $functionStatus = 2; // calculating
-
-            if ($passable->status > $functionStatus) {
-                return $passable;
-            }
-
-            $passable->status = $functionStatus;
-            dump($passable->status);
-
-            $passable->total = collect($passable->line_items)->sum(function ($item) {
-                return $item['price'] * $item['quantity'];
-            });
-            return $passable;
-        })
-        ->pipe(function ($passable) {
-            $functionStatus = 3; // calculating
-
-            if ($passable->status > $functionStatus) {
-                return $passable;
-            }
-
-            $passable->status = $functionStatus;
-
-            throw new Exception('Something went wrong');
-
-            return $passable;
-        })
-        ->pipe(function ($passable) {
-            $functionStatus = 4; // completed
-
-            if ($passable->status > $functionStatus) {
-                return $passable;
-            }
-
-            $passable->status = $functionStatus;
-            dump($passable->status);
-            return $passable;
-        });
-
-    try {
-        $response = $pipeline->process($order);
-    } catch(Exception $e) {
-        $response = $e->getMessage();
-    }
-
-    dd($pipeline , $response, $order);
-});
-
-// Testing Laravel Pipelines with league/pipeline
-// Testing with PipelineBuilder
-Route::get('/pipeline/3', function () {
-    // Prepare the builder
-    $pipelineBuilder = (new PipelineBuilder)
-        ->add(function ($passable) {
-            return $passable . ' Hello';
-        })
-        ->add(function ($passable) {
-            return $passable . ' World';
-        });
-
-    // Build the pipeline
-    $pipeline = $pipelineBuilder->build();
-
-    dd($pipelineBuilder, $pipeline, $pipeline->process(''));
-});
+require __DIR__.'/auth.php';
